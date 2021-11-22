3a10415311fee4891cdb1243ad88500356df36e5
Howun Lam <howunlam96@gmail.com>
2021-11-22 17:07:09 +0000

    Translator is able to translate individual morse code into letters

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ const translateButton = document.getElementById("translateButton");
 let textBox = document.getElementById("translateTxt");
 let translatedTxt = document.getElementById("translated");
 
+// const splitted = textBox.split(" ");
+let inputValues = (textBox.value)
+let test1 = "";
+test1 = inputValues.toString();
+
 
 translateButton.addEventListener("click", () => {
   console.log(textBox.value);
   console.log(morse(textBox.value))
   translatedTxt.innerHTML = morse(textBox.value)
+
 })
