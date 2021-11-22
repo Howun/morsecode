d0a5b411bd07f9977b9070fc0529ddf5fa14d254
Howun Lam <howunlam96@gmail.com>
2021-11-22 15:56:23 +0000

    able to translate letters

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import {morse} from "./translator.js";
 
+const translateButton = document.getElementById("translateButton");
+let textBox = document.getElementById("translateTxt");
+let translatedTxt = document.getElementById("translated");
 
 
-console.log(morse ("..."), morse ("---"), morse ("..."));
-console.log(morse("--..."));
+translateButton.addEventListener("click", () => {
+  console.log(textBox.value);
+  console.log(morse(textBox.value))
+  translatedTxt.innerHTML = morse(textBox.value)
+})
