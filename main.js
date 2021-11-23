c5724fe1a8d1cd2c978296d2bc83f7e4efa63c55
Howun Lam <howunlam96@gmail.com>
2021-11-23 17:59:11 +0000

    Fully working translator from Morse code to English. Still needs styling done and to make English to Morse code, but it is fully functional.

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const translateButton = document.getElementById("translateButton");
 let textBox = document.getElementById("translateTxt");
 let translatedTxt = document.getElementById("translated");
 
-// const splitted = textBox.split(" ");
-let inputValues = (textBox.value)
-let test1 = "";
-test1 = inputValues.toString();
-
-
 translateButton.addEventListener("click", () => {
-  console.log(textBox.value);
-  console.log(morse(textBox.value))
-  translatedTxt.innerHTML = morse(textBox.value)
 
-})
+  let toArr = textBox.value.split(" ");
+
+  for (let i = 0; i < toArr.length; i++){
+    const translated = toArr.map(values => {
+      return morse(values)
+    })
+
+    translatedTxt.innerHTML = translated.join("");
+  }
+})                                          
